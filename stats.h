84bf636dd379a5016584617ca892d5bedc979c3e
Sowndarya-code <77658898+Sowndarya-code@users.noreply.github.com>
2021-01-28 11:27:26 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -4,7 +4,8 @@ namespace Statistics {
     class Stats()
     {
         public:
-        Stats ComputeStatistics(const std::vector<float>& );
+        Stats ComputeStatistics(const std::vector<float>& S2);
+        vector<float> S1;
         float average,min,max;
     };
 }
