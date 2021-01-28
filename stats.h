1ab6eb65d52675ef97f56c71e25b13876f60ce83
Sowndarya-code <77658898+Sowndarya-code@users.noreply.github.com>
2021-01-28 14:27:01 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -19,4 +19,18 @@ namespace Statistics {
         public:
         bool ledGlows;
     };
+    class IAlerter
+    {
+        virtual void checkAndAlert(std::vector<float>) = 0;
+    };
+    class StatsAlerter : public IAlerter
+    {
+        public:
+        StatsAlerter (float f_max_thres, std::vector<IAlerter*> &f_Alerters) : m_thresh(f_max_thres),m_Alerters(f_Alerters) {}
+        //std::vector<float> S3;
+        void checkAndAlert(std::vector<float> S3) override;
+        float m_thresh;
+       std::vector<IAlerter*> *m_Alerters = {&emailAlert, &ledAlert};
+       
+    };
 }
