bb9c045301557a794161d9bffc1004a89790abb2
Sowndarya-code <77658898+Sowndarya-code@users.noreply.github.com>
2021-01-28 19:07:36 +0530

    Final commit

--- stats-test.cpp
+++ stats-test.cpp
@@ -23,7 +23,7 @@ TEST_CASE("average is NaN for empty array") {
     //Use http://www.cplusplus.com/reference/cmath/isnan/
     static Statistics::Stats computedStats;
     computedStats.ComputeStatistics({});
-    REQUIRE(std::abs(isnan(computedStats.average));
+    REQUIRE(std::abs(isnan(computedStats.average)));
 }
 
 TEST_CASE("raises alerts when max is greater than threshold") {
