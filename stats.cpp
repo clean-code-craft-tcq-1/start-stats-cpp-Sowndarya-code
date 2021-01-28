1546869898269ecbab9c64cf4baff4eff1a68be2
Sowndarya-code <77658898+Sowndarya-code@users.noreply.github.com>
2021-01-28 11:56:31 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,10 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<float>& S2) {
+    for(auto int i=0,i<S2.size(),i++)
+    {   
+      sum = sum + S2[i];
+    }
+    average = sum/S2.size();
     //Implement statistics here
 }
