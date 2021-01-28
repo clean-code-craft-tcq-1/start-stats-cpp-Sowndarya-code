1435e8eccb07fa8fbe67a57764b9abea866152cc
Sowndarya-code <77658898+Sowndarya-code@users.noreply.github.com>
2021-01-28 19:34:41 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -23,6 +23,7 @@ TEST_CASE("average is NaN for empty array") {
     //Use http://www.cplusplus.com/reference/cmath/isnan/
     static Statistics::Stats computedStats;
     std::vector<float> empty_vector;
+    empty_vector.empty();
     computedStats.ComputeStatistics(empty_vector);
     REQUIRE(std::isnan(computedStats.average));
     REQUIRE(std::isnan(computedStats.max));
