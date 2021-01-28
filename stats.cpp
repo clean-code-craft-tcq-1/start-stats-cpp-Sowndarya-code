31e9ae7011bf7f6353d8c523b2ad708bb0c533cc
Sowndarya-code <77658898+Sowndarya-code@users.noreply.github.com>
2021-01-28 13:06:15 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,14 +1,14 @@
 #include "stats.h"
-
+using namespace std;
 Statistics::Stats Statistics::Stats::ComputeStatistics(const std::vector<float>& S2) 
 {
-    for(uint i=0; i<S2.size(); i++)
+    for(int i=0; i<S2.size(); i++)
     {   
       sum = sum + S2[i];
     }
     average = sum/S2.size();
-    min = *std::min_element(S2.begin(),S2.end());
-    max = *std::max_element(S2.begin(),S2.end());
+    min = *min_element(S2.begin(),S2.end());
+    max = *max_element(S2.begin(),S2.end());
     return *this;
     //Implement statistics here
 }
